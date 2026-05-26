cfc5cb6787ec4d3f3598fea1663dd7647848e226
msdak <msdak@mahi>
2026-05-26 15:14:01 +0530

    Added @EnableMethodSecurity to SecurityConfig to enable @PreAuthorize on controllers

--- ecommerce/src/main/java/com/ecommerce/config/SecurityConfig.java
+++ ecommerce/src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ecommerce.config;
 import org.springframework.context.annotation.*;
 import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
