1f90dd10f552619302b6719afc73c29f4bb9a60a
Suraj <surajdhurve0096@gmail.com>
2026-05-28 12:32:53 +0530

    SCRUM-7: Remove duplicate getters/setters from model classes

--- ecommerce/src/main/java/com/ecommerce/model/CartItem.java
+++ ecommerce/src/main/java/com/ecommerce/model/CartItem.java
@@ -10,38 +10,6 @@ public class CartItem {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Cart getCart() {
-		return cart;
-	}
-
-	public void setCart(Cart cart) {
-		this.cart = cart;
-	}
-
-	public Product getProduct() {
-		return product;
-	}
-
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
 	@ManyToOne
     @JoinColumn(name = "cart_id")
     private Cart cart;
--- ecommerce/src/main/java/com/ecommerce/model/Order.java
+++ ecommerce/src/main/java/com/ecommerce/model/Order.java
@@ -50,61 +50,6 @@ public class Order {
 
     private String shippingAddress;
 
-    public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public List<OrderItem> getItems() {
-		return items;
-	}
-
-	public void setItems(List<OrderItem> items) {
-		this.items = items;
-	}
-
-	public BigDecimal getTotalAmount() {
-		return totalAmount;
-	}
-
-	public void setTotalAmount(BigDecimal totalAmount) {
-		this.totalAmount = totalAmount;
-	}
-
-	public OrderStatus getStatus() {
-		return status;
-	}
-
-	public void setStatus(OrderStatus status) {
-		this.status = status;
-	}
-
-	public String getShippingAddress() {
-		return shippingAddress;
-	}
-
-	public void setShippingAddress(String shippingAddress) {
-		this.shippingAddress = shippingAddress;
-	}
-
-	public LocalDateTime getCreatedAt() {
-		return createdAt1;
-	}
-
-	public void setCreatedAt(LocalDateTime createdAt) {
-		this.createdAt1 = createdAt;
-	}
 
 	@Column(updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
